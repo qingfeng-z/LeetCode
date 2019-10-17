ff74c7c5fba354cb40845cacf77ef01159c2d790
清风 <qingfeng@shouhouyi.net>
2019-10-17 13:01:17 +0800

    HelloGitHub

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,6 +7,7 @@ namespace LeetCode
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
